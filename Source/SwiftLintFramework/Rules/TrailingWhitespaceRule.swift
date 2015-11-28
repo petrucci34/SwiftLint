828cf069264e1895b42d1b9a6b7a7b6682c0a233
JP Simard <jp@jpsim.com>
2015-11-27 17:44:04 -0800

    autocorrect trailing whitespace violations

--- Source/SwiftLintFramework/Rules/TrailingWhitespaceRule.swift
+++ Source/SwiftLintFramework/Rules/TrailingWhitespaceRule.swift
@@ -8,7 +8,7 @@
 
 import SourceKittenFramework
 
-public struct TrailingWhitespaceRule: Rule {
+public struct TrailingWhitespaceRule: CorrectableRule {
     public init() {}
 
     public static let description = RuleDescription(
@@ -16,7 +16,8 @@ public struct TrailingWhitespaceRule: Rule {
         name: "Trailing Whitespace",
         description: "Lines should not have trailing whitespace.",
         nonTriggeringExamples: [ "//\n" ],
-        triggeringExamples: [ "// \n" ]
+        triggeringExamples: [ "// \n" ],
+        corrections: [ "// \n": "//\n" ]
     )
 
     public func validateFile(file: File) -> [StyleViolation] {
@@ -25,6 +26,16 @@ public struct TrailingWhitespaceRule: Rule {
         }.map {
             StyleViolation(ruleDescription: self.dynamicType.description,
                 location: Location(file: file.path, line: $0.index))
+        }
+    }
+
+    public func correctFile(file: File) {
+        let whitespaceCharacterSet = NSCharacterSet.whitespaceCharacterSet()
+        let correctedContents = file.lines.map {
+            ($0.content as NSString).stringByTrimmingTrailingCharactersInSet(whitespaceCharacterSet)
+        }.joinWithSeparator("\n") + "\n" // re-add trailing newline
+        if correctedContents != file.contents {
+            file.write(correctedContents)
         }
     }
 }
